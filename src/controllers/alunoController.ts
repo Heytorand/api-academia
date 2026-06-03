f87e777ff25ad7ad084e0c3ba1fcd0b7ae4bf839
Weyd <weydlll2606@gmail.com>
2026-06-03 11:35:25 -0300

    implementa controller de alunos com tratamento de erros via next()

--- src/controllers/alunoController.ts
+++ src/controllers/alunoController.ts
@@ -1,21 +1,114 @@
-// ============================================================
-// 🎯 TAREFA: Crie o Controller com try/catch + next(erro)!
-// ============================================================
-//
-// import { Request, Response, NextFunction } from "express";
-// import { AlunoModel } from "../models/alunoModel";
-// import { ApiResponse, AlunoParams, FiltroQuery, CriarAlunoDTO } from "../interfaces";
-// import { Aluno } from "../entities/Aluno";
-//
-// const model = new AlunoModel();
-//
-// 5 funções API (try/catch + next):
-//   listar:      model.listarTodos() + filtro por plano e ativo
-//   buscarPorId: model.buscarPorId(Number(req.params.id))
-//   criar:       model.criar(req.body as CriarAlunoDTO) → 201
-//   atualizar:   model.atualizarAluno(id, req.body)
-//   remover:     model.remover(id)
-//
-// O Model usa Aluno.validar() → throw. Controller NÃO valida!
-// ============================================================
+import { Request, Response, NextFunction } from "express";
+import { AlunoModel } from "../models/alunoModel";
+import {
+  ApiResponse,
+  AlunoParams,
+  FiltroQuery,
+  CriarAlunoDTO,
+  AtualizarAlunoDTO,
+} from "../interfaces";
+import { Aluno } from "../entities/Aluno";
 
+const model = new AlunoModel();
+
+export const listar = async (
+  req: Request<{}, {}, {}, FiltroQuery>,
+  res: Response<ApiResponse<Aluno[]>>,
+  next: NextFunction
+) => {
+  try {
+    let alunos = await model.listarTodos();
+
+    if (req.query.plano) {
+      alunos = alunos.filter(
+        (aluno) => aluno.plano === req.query.plano
+      );
+    }
+
+    if (req.query.ativo !== undefined) {
+      const ativo = req.query.ativo === "true";
+
+      alunos = alunos.filter(
+        (aluno: any) => aluno.ativo === ativo
+      );
+    }
+
+    res.json({
+      sucesso: true,
+      dados: alunos,
+    });
+  } catch (erro) {
+    next(erro);
+  }
+};
+
+export const buscarPorId = async (
+  req: Request<AlunoParams>,
+  res: Response<ApiResponse<Aluno>>,
+  next: NextFunction
+) => {
+  try {
+    const aluno = await model.buscarPorId(Number(req.params.id));
+
+    res.json({
+      sucesso: true,
+      dados: aluno,
+    });
+  } catch (erro) {
+    next(erro);
+  }
+};
+
+export const criar = async (
+  req: Request<{}, {}, CriarAlunoDTO>,
+  res: Response<ApiResponse<Aluno>>,
+  next: NextFunction
+) => {
+  try {
+    const aluno = await model.criar(req.body);
+
+    res.status(201).json({
+      sucesso: true,
+      dados: aluno,
+    });
+  } catch (erro) {
+    next(erro);
+  }
+};
+
+export const atualizar = async (
+  req: Request<AlunoParams, {}, AtualizarAlunoDTO>,
+  res: Response<ApiResponse<Aluno>>,
+  next: NextFunction
+) => {
+  try {
+    const aluno = await model.atualizarAluno(
+      Number(req.params.id),
+      req.body
+    );
+
+    res.json({
+      sucesso: true,
+      dados: aluno,
+    });
+  } catch (erro) {
+    next(erro);
+  }
+};
+
+export const remover = async (
+  req: Request<AlunoParams>,
+  res: Response<ApiResponse<null>>,
+  next: NextFunction
+) => {
+  try {
+    await model.remover(Number(req.params.id));
+
+    res.json({
+      sucesso: true,
+      dados: null,
+    });
+  } catch (erro) {
+    next(erro);
+  }
+};
